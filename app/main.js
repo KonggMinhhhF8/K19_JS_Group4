eea9086e8ee40a0fc58c8ae76cf08aaee298df3c
Vo Pham Viet Phu <vietphummo205@gmail.com>
2026-07-01 08:44:30 +0700

    Initial commit

--- app/main.js
+++ app/main.js
@@ -1,49 +1,51 @@
 import {
-    HomePage,
-    LoginPage,
-    ProductsPage,
-    CustomersPage,
-    OrdersPage,
-    ReportsPage,
+	HomePage,
+	LoginPage,
+	ProductsPage,
+	CustomersPage,
+	OrdersPage,
+	ReportsPage,
 } from "./pages";
 import { requireAuth } from "./api/auth.js";
 import router from "./plugins/router.js";
+import "./assets/css/style.css";
+import "./assets/css/reports.css";
 
 const accessToken = localStorage.getItem("accessToken");
 const refreshToken = localStorage.getItem("refreshToken");
 
 router.on("", function () {
-    requireAuth(() => {
-        HomePage();
-    });
+	requireAuth(() => {
+		HomePage();
+	});
 });
 
 router.on("/login", function () {
-    LoginPage();
+	LoginPage();
 });
 
 router.on("/products", function () {
-    requireAuth(() => {
-        ProductsPage();
-    });
+	requireAuth(() => {
+		ProductsPage();
+	});
 });
 
 router.on("/customers", function () {
-    requireAuth(() => {
-        CustomersPage();
-    });
+	requireAuth(() => {
+		CustomersPage();
+	});
 });
 
 router.on("/orders", function () {
-    requireAuth(() => {
-        OrdersPage();
-    });
+	requireAuth(() => {
+		OrdersPage();
+	});
 });
 
 router.on("/reports", function () {
-    requireAuth(() => {
-        ReportsPage();
-    });
+	requireAuth(() => {
+		ReportsPage();
+	});
 });
 
 router.resolve();
