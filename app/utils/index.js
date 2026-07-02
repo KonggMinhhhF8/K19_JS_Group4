7790118fb89a29081f87f2565ed42bd6aa847c82
duynkhv-F8 <258801460+duynkhv-F8@users.noreply.github.com>
2026-07-02 22:21:45 +0700

    feat(sidebar): creat function sidebar and export it by utils/index.js

--- app/utils/index.js
+++ app/utils/index.js
@@ -2,6 +2,7 @@ export { renderHeader } from "./renderHeader/index.js";
 export { renderStats } from "./renderStats/index.js";
 export { renderTable } from "./renderTable/index.js";
 export { renderFilter } from "./renderFilter/index.js";
+export {renderSidebar} from "./renderSidebar/index.js";
 
 export { productHeader } from "./const/product.js";
 export { customerHeader } from "./const/customer.js";
